99e89a1a8d38551db46d01181d733b852b8e583b
Daniella <dansmc.courses@gmail.com>
2023-03-01 20:04:13 -0600

    First test

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,24 +1,26 @@
 const { default: expect } = require('expect');
 
 const calculator = require('../calculator');
-
+// //test('adds 1 + 2 to equal 3', () => {
+//     expect(sum(1, 2)).toBe(3);
+// });
 test('adds a + b', () => {
-    //
+    expect(sum(1, 2)).toBe(3);
 })
 
 test('substracts a - b', () => {
-   //
+    expect(substract(4,1)).toBe(3);
 })
 
 test('multiplies a * b', () => {
-    //
+    expect(multiply(3, 3)).toBe(9);
 })
 
 test('divides a / b', () => {
-    // Consider decimals
+    expect(divide(6, 2)).toBe(3);
 })
 
 test('Raises to power a ^ b', () => {
-    //
+    expect(power(5, 5)).toBe(25);
 })
 
